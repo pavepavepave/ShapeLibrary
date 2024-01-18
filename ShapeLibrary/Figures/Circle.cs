89cc043a7efa08839395090e73a44df8b987d991
Pavel Yakovlev <deadamag@mail.ru>
2024-01-18 23:03:56 +0300

    Added classes figures: circle, triangle

---
+++ ShapeLibrary/Figures/Circle.cs
@@ -0,0 +1,22 @@
+﻿using ShapeLibrary.Interfaces;
+
+namespace ShapeLibrary.Figures
+{
+    public class Circle : IFigure
+    {
+        private readonly double Radius;
+
+        public Circle(double radius)
+        {
+            if (radius < 0)
+                throw new Exception("Значение не может быть меньше 0.");
+
+            Radius = radius;
+        }
+
+        public double CalculateArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+    }
+}
---
+++ ShapeLibrary/Figures/Triangle.cs
@@ -0,0 +1,38 @@
+﻿using ShapeLibrary.Interfaces;
+
+namespace ShapeLibrary.Figures
+{
+    public class Triangle : IFigure
+    {
+        private readonly double SideA;
+        private readonly double SideB;
+        private readonly double SideC;
+
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            if (sideA < 0 || sideB < 0 || sideC < 0)
+                throw new Exception("Значение не может быть меньше 0.");
+
+            SideA = sideA;
+            SideB = sideB;
+            SideC = sideC;
+        }
+
+        public double CalculateArea()
+        {
+            var semiperimeter = (SideA + SideB + SideC) / 2;
+            return Math.Sqrt(semiperimeter * (semiperimeter - SideA) * (semiperimeter - SideB) * (semiperimeter - SideC));
+        }
+
+        /// <summary>
+        /// Checking if the triangle is rectangular
+        /// </summary>
+        /// <returns></returns>
+        public bool IsTriangleRectangular()
+        {
+            var sidesArray = new[] { SideA, SideB, SideC };
+            Array.Sort(sidesArray);
+            return Math.Pow(sidesArray[0], 2) + Math.Pow(sidesArray[1], 2) == Math.Pow(sidesArray[2], 2);
+        }
+    }
+}
