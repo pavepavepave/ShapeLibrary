7367867f290474afc357b3bcfb0c735decb5f45e
Pavel Yakovlev <deadamag@mail.ru>
2024-01-19 01:26:34 +0300

    Implemented figure creation method

--- ShapeLibrary/ShapeFactory.cs
+++ ShapeLibrary/ShapeFactory.cs
@@ -1,7 +1,18 @@
-﻿namespace ShapeLibrary
-{
-    public class ShapeFactory
-    {
+﻿using ShapeLibrary.Figures;
+using ShapeLibrary.Interfaces;
 
+namespace ShapeLibrary
+{
+    public static class ShapeFactory
+    {
+        public static IFigure? Create(params double[] args)
+        {
+            return args.Length switch
+            {
+                1 => new Circle(args[0]),
+                3 => new Triangle(args[0], args[1], args[2]),
+                _ => null,
+            };
+        }
     }
 }
