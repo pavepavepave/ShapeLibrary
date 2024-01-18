0d2aa5e11661d11eb31ff245d77ee1116aa85e88
Pavel Yakovlev <deadamag@mail.ru>
2024-01-18 22:03:26 +0300

    Added interface for figures

---
+++ ShapeLibrary/Interfaces/IFigure.cs
@@ -0,0 +1,11 @@
+﻿namespace ShapeLibrary.Interfaces
+{
+    public interface IFigure
+    {
+        /// <summary>
+        /// Calculating the area of ​​shapes
+        /// </summary>
+        /// <returns></returns>
+        public double CalculateArea();
+    }
+}
